ed0e64fa3c92fea95d5574b1d7d79fc43e672add
ju01n <wjddls1222@gmail.com>
2023-09-04 18:24:37 +0900

    [study] 스터디 업데이트, 인덱스 정리

--- section11/js/wall3d.js
+++ section11/js/wall3d.js
@@ -1,44 +1,39 @@
 (function () {
-    const stageElem = document.querySelector('.stage');
-    const houseElem = document.querySelector('.house');
-    const barElem = document.querySelector('.progress-bar');
-    const selectCharacterElem = document.querySelector('.select-character');
-    const mousePos = { x: 0, y: 0 };
-    let maxScrollValue;
+  const stageElem = document.querySelector('.stage');
+  const houseElem = document.querySelector('.house');
+  const barElem = document.querySelector('.progress-bar');
+  const mousePos = { x: 0, y: 0 };
+  let maxScrollValue;
+  // 현재 창 사이즈 (전체 스크롤 할 수 있는 범위)
 
-    function resizeHandler() {
-        maxScrollValue = document.body.offsetHeight - window.innerHeight;
-    }
+  // 바뀐 창 사이즈에 따라 리사이징
+  function resizeHandler() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
 
-    window.addEventListener('scroll', function () {
-        const scrollPer = pageYOffset / maxScrollValue;
-        const zMove = scrollPer * 980 - 490;
-        houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+  window.addEventListener('scroll', function () {
+    const scrollPer = pageYOffset / maxScrollValue;
+    // console.log(pageYOffset);
+    // console.log(pageYOffset / maxScrollValue); // 이 비율에 따라 조정하면 될듯
+    // 현재 창 사이즈 (전체 스크롤 할 수 있는 범위)
+    const zMove = scrollPer * 980 - 490;
+    // 490 : house 디폴트값이 -490이여서
+    houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
 
-        // progress bar
-        barElem.style.width = scrollPer * 100 + '%';
-    });
+    // progress bar
+    barElem.style.width = scrollPer * 100 + '%';
+  });
 
-    window.addEventListener('mousemove', function (e) {
-        mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
-        mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
-        stageElem.style.transform = 'rotateX(' + (mousePos.y * 5) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
-    });
+  window.addEventListener('mousemove', function (e) {
+    // console.log(e.clientX, e.clientY); // 마우스의 위치값
 
-    window.addEventListener('resize', resizeHandler);
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
+    // console.log(mousePos);
+    stageElem.style.transform =
+      'rotateX(' + mousePos.y * 5 + 'deg) rotateY(' + mousePos.x * 5 + 'deg)';
+  });
 
-    stageElem.addEventListener('click', function (e) {
-        new Character({
-            xPos: e.clientX / window.innerWidth * 100,
-            speed: Math.random() * 0.5 + 0.2
-        });
-    });
-
-    selectCharacterElem.addEventListener('click', function (e) {
-        const value = e.target.getAttribute('data-char');
-        document.body.setAttribute('data-char', value);
-    });
-
-    resizeHandler();
-
-})();
+  window.addEventListener('resize', resizeHandler);
+  resizeHandler();
+})(); // 지우지마
